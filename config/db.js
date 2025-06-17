e42e92bb5415d53503fdde0df687ed2bacc08f15
saiimoon <simoonperez04@gmail.com>
2025-06-16 21:33:55 -0300

    correccion de la base sqlite

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
+const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const { open } = require('sqlite');
-const path = require('path');
 
-async function connectDB() {
+async function getDB() {
   return open({
-    filename: path.join(__dirname, 'init_sqlite.sqlite'),
+    filename: path.join(__dirname, '../data/init_sqlite.sqlite'),
     driver: sqlite3.Database
   });
 }
 
-module.exports = connectDB;
+module.exports = getDB;
